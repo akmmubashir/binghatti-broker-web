a38fc30b2b1c69ccdc32424b8ea880fae7e613d6
akmmubashir <akmmubashir@gmail.com>
2026-01-18 00:38:35 +0530

    header and footer updation

--- app/components/header.tsx
+++ app/components/header.tsx
@@ -18,7 +18,7 @@ const Header = () => {
       className={`'flex justify-center fixed w-full left-0 top-0 z-50 ${scrolled ? "bg-gray-950/20 backdrop-blur-[3px]" : "bg-transparent"} `}
     >
       <div
-        className={`p-[40px_80px] max-xl:p-[30px_40px] max-md:p-[20px_20px] flex items-center justify-between`}
+        className={`p-[40px_100px] max-xl:p-[30px_60px] max-md:p-[20px_20px] flex items-center justify-between`}
       >
         <Logo color={scrolled ? "black" : "white"} />
       </div>
