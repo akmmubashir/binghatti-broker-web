c866ad8cb41270f096b2df9c4db814eb5b433f28
akmmubashir <akmmubashir@gmail.com>
2026-01-18 16:36:42 +0530

    changes

--- app/home/components/projectList.tsx
+++ app/home/components/projectList.tsx
@@ -139,7 +139,7 @@ const ProjectList = () => {
   return (
     <section
       ref={containerRef}
-      className="snap-section relative h-dvh overflow-hidden bg-black"
+      className="snap-section relative h-dvh overflow-hidden bg-black bg-no-repeat bg-cover bg-center"
       style={{
         backgroundImage: `url(${currentProject.image})`,
         // filter: "grayscale(0%) brightness(80%)",
